ddc30c88e75bded499e3a233bd56af9512267341
Cybandy <andyboat75@gmail.com>
2023-11-12 01:50:12 +0100

    stripe setup

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  // layers
-  extends: [
-    '../v2Frontend'
-  ]
+  modules: [
+    '@unlok-co/nuxt-stripe'
+  ],
+  stripe:{
+    client:{
+      key: process.env.STRIPE_API_PK
+    }
+  },
 })
