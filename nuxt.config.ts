19438eaa6a0fe28c3d8e29bbf2fdf0891400d962
Cybandy <andyboat75@gmail.com>
2024-02-26 12:46:55 +0100

    enabled crossOrigin attribute for the source

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  app:{
-    head:{
-     script: [
-      {src: 'https://js.stripe.com/v3'}
-     ]
+  app: {
+    head: {
+      script: [
+        { src: 'https://js.stripe.com/v3', crossorigin: 'anonymous' }
+      ]
     }
   },
   modules: [
