647c336afdf0d19f291725a9a16b04f23d40d88e
cybandy <andyboat75@gmail.com>
2025-04-05 19:21:04 +0200

    upgrade

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,19 +2,4 @@
 export default defineNuxtConfig({
   devtools: { enabled: process.env.NODE_ENV !== 'production' },
   compatibilityDate: '2024-04-03',
-  app: {
-    head: {
-      // script: [
-      //   { src: 'https://js.stripe.com/v3', crossorigin: 'anonymous' }
-      // ]
-    }
-  },
-  modules: [
-    // '@unlok-co/nuxt-stripe'
-  ],
-  // stripe:{
-  //   client:{
-  //     key: process.env.STRIPE_API_PK
-  //   }
-  // },
 })
